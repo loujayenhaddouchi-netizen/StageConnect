fca16cbe8bad5fcd9e6b8a0bc8b8441c137958ac
loujayenhaddouchi-netizen <loujayenhaddouchi@gmail.com>
2026-04-26 01:32:52 +0200

    Update Utilisateur.java

--- src/com/stageconnect/models/src/com/stageconnect/models/Utilisateur.java
+++ src/com/stageconnect/models/src/com/stageconnect/models/Utilisateur.java
@@ -1,1 +1,37 @@
 
+package com.stageconnect.models;
+
+public class Utilisateur {
+
+    private String nom;
+    private String email;
+
+    public Utilisateur() {
+    }
+
+    public Utilisateur(String nom, String email) {
+        this.nom = nom;
+        this.email = email;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void afficherDetails() {
+        System.out.println("Nom: " + nom);
+        System.out.println("Email: " + email);
+    }
+}
